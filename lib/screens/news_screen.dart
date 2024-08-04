af05865e5a6b21e21c4eded6acfd1ac500c316c4
Sepehr <sepehrfakoori@gmail.com>
2024-08-04 20:19:57 +0330

    Create News Screen

--- lib/screens/news_screen.dart
+++ lib/screens/news_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:ui';
 
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:news_flutter_application/data/models/news_article.dart';
 
@@ -24,6 +25,25 @@ class _NewsScreenState extends State<NewsScreen> {
               slivers: [
                 SliverAppBar(
                   toolbarHeight: 70,
+                  leading: Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.all(8),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Colors.grey.withOpacity(0.7),
+                        ),
+                        child: ImageFiltered(
+                          imageFilter: ImageFilter.blur(sigmaY: 10, sigmaX: 10),
+                          child: const Icon(Icons.shopping_cart_outlined),
+                        ),
+                      ),
+                      const BackButton(
+                        color: Colors.white,
+                      ),
+                    ],
+                  ),
                   title: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
@@ -81,9 +101,23 @@ class _NewsScreenState extends State<NewsScreen> {
                             topLeft: Radius.circular(20)),
                         color: Colors.white,
                       ),
-                      padding: const EdgeInsets.only(top: 5, bottom: 10),
-                      child: const Center(
-                        child: Text("Sliver App Bar"),
+                      padding: const EdgeInsets.only(
+                        top: 5,
+                        bottom: 10,
+                        left: 20,
+                        right: 20,
+                      ),
+                      child: Center(
+                        child: Text(
+                          widget.newsArticle.title!,
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                          textAlign: TextAlign.center,
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                          ),
+                        ),
                       ),
                     ),
                   ),
@@ -92,18 +126,30 @@ class _NewsScreenState extends State<NewsScreen> {
                   backgroundColor: Colors.white,
                   surfaceTintColor: Colors.transparent,
                   flexibleSpace: FlexibleSpaceBar(
-                    background: Image.asset(
-                      "assets/images/news_image.jpg",
-                      width: double.maxFinite,
-                      fit: BoxFit.cover,
-                    ),
+                    background: widget.newsArticle.urlToImage == null
+                        ? Image.asset(
+                            "assets/images/news_image.jpg",
+                            width: double.maxFinite,
+                            fit: BoxFit.cover,
+                          )
+                        : CachedNetworkImage(
+                            imageUrl: widget.newsArticle.urlToImage!,
+                            fit: BoxFit.cover,
+                          ),
                   ),
                 ),
                 SliverToBoxAdapter(
                   child: Padding(
                     padding: const EdgeInsets.only(
                         left: 20.0, right: 20, top: 20, bottom: 100),
-                    child: Text(widget.newsArticle.description.toString()),
+                    child: Text(
+                      widget.newsArticle.content.toString()*10,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                      textAlign: TextAlign.justify,
+                    ),
                   ),
                 ),
               ],
